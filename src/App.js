eed3d61ab0beb3c58965bcbab3ea45674b1760cb
RizkiB12 <rbagus306@gmail.com>
2023-06-15 17:47:54 +0700

    update: add taildwind and react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          GOGOGOGO <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-1 gap-5">
+      <div className="flex flex-col min-h-screen p-5 bg-gray-100 w-full lg:w-1/3 md:w-1/2 justify-self-center">
+        <div className="mb-auto space-y-5"></div>
+      </div>
     </div>
   );
 }
